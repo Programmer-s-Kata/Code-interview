7ad87190b7fe3ccec0e13e9686aad067cbd1818f
Viktor <victordm0394@gmail.com>
2024-12-05 23:58:51 +0200

    Start the insertion sort implementation.

---
+++ algorithms/src/main/java/sorts/insertion/InsertionSort.java
@@ -0,0 +1,11 @@
+package sorts.insertion;
+
+import sorts.common.SortAlgorithm;
+
+public class InsertionSort implements SortAlgorithm {
+
+    @Override
+    public <T extends Comparable<T>> T[] sort(T[] unsorted) {
+        return null;
+    }
+}
