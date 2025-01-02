166d43bb521e060cd3439a04a0b0a14f4a3d421c
Viktor <victordm0394@gmail.com>
2025-01-03 00:00:44 +0200

    Update gradle config-2

---
+++ leetcode/build.gradle.kts
@@ -0,0 +1,21 @@
+import dependency.JUnit
+
+plugins {
+    `java-library`
+}
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(JUnit.engine)
+    testImplementation(platform(JUnit.bom))
+    JUnit.api.forEach{
+        testImplementation(it)
+    }
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
